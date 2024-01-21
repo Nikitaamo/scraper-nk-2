53545cc0d653e8738db0675825b2d2940e56d99a
Nikitaamo <156019234+Nikitaamo@users.noreply.github.com>
2024-01-21 23:35:11 +0200

    Scraper without README.md

--- scraper2/scraper.py
+++ scraper2/scraper.py
@@ -2,9 +2,9 @@ import os
 import json
 import logging
 import time
+import re
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-
 from utils import get_html, parse_html
 
 # Load configuration
@@ -16,54 +16,77 @@ log_directory = "logs"
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 
-log_file_path = os.path.join(log_directory, 'main.log')
+# Set up main logger
+main_log_file_path = os.path.join(log_directory, 'main.log')
+main_logger = logging.getLogger('MainLogger')
+main_logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(main_log_file_path)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+file_handler.setFormatter(formatter)
+main_logger.addHandler(file_handler)
 
-# Set up logging
-logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
-logging.info("Logging is set up. Starting the scraper.")
+# Set up job details logger
+job_details_log_file_path = os.path.join(log_directory, 'job_details.log')
+job_details_logger = logging.getLogger('JobDetailsLogger')
+job_details_logger.setLevel(logging.INFO)
+details_file_handler = logging.FileHandler(job_details_log_file_path)
+details_file_handler.setFormatter(formatter)
+job_details_logger.addHandler(details_file_handler)
+
+main_logger.info("Logging is set up. Starting the scraper.")
 
 # Get the HTML content of the search page
-logging.info(f"Fetching search results from {config['search_url']}")
+main_logger.info(f"Fetching search results from {config['search_url']}")
 search_html = get_html(config['search_url'])
 
 if search_html:
-    logging.info("Search page fetched successfully.")
+    main_logger.info("Search page fetched successfully.")
     job_elements = parse_html(search_html, config['job_list_selector'])
-    logging.info(f"Found {len(job_elements)} job elements.")
+    main_logger.info(f"Found {len(job_elements)} job elements.")
+    job_details_logger.info(f"Found {len(job_elements)} job elements.")
 
-    for job_element in job_elements:
-        # Assuming the job link is within an 'a' tag directly within each job element.
+    for i, job_element in enumerate(job_elements, start=1):
         job_link_element = job_element.find('a', href=True)
         if job_link_element:
             job_url = urljoin(config['base_url'], job_link_element['href'])
-            logging.info(f"Fetching job details from {job_url}")
+            main_logger.info(f"Fetching job details from {job_url}")
             job_html = get_html(job_url)
 
             if job_html:
                 job_soup = BeautifulSoup(job_html, 'html.parser')
                 title_element = job_soup.select_one(config['job_title_selector'])
+                title = title_element.get_text(strip=True) if title_element else 'Title Not Found'
+
                 location_element = job_soup.select_one(config['job_location_selector'])
-                company_element = job_soup.select_one(config['job_company_selector'])
+                company_element = job_element.select_one(config['job_company_selector'])
                 views_element = job_soup.select_one(config['job_views_selector'])
                 applied_element = job_soup.select_one(config['job_apply_selector'])
+                salary_element = job_soup.select_one(config['job_salary_selector'])
 
-                title = title_element.get_text(strip=True) if title_element else 'Not provided'
-                location = location_element.get_text(strip=True).split('-')[
-                    0].strip() if location_element else 'Not provided'
-                company = company_element.get_text(strip=True) if company_element else 'Not provided'
-                views = views_element.get_text(strip=True) if views_element else 'Not provided'
-                applied = applied_element.get_text(strip=True) if applied_element else 'Not provided'
+                location = location_element.get_text(strip=True).split('-')[0].strip() if location_element else 'Location Not Provided'
+                company = company_element.get_text(strip=True) if company_element else 'Company Not Provided'
+                views = views_element.get_text(strip=True) if views_element else 'Views Not Provided'
+                applied = applied_element.get_text(strip=True) if applied_element else 'Applications Not Provided'
+                salary_text = salary_element.get_text(strip=True) if salary_element else 'Salary Not Provided'
 
-                logging.info(
-                    f"Job Title: {title}, Location: {location}, Company: {company}, Views: {views}, Applied: {applied}")
-                print(f"Scraped: {title}")
+                salary = 'Salary Not Provided'
+                if salary_text:
+                    salary_numbers = re.findall(r'\b\d+\b', salary_text)
+                    if salary_numbers and 'Gross' in salary_text:
+                        salary_net = [str(int(round(int(num) * 0.6))) for num in salary_numbers]  # Calculate net salary
+                        salary = '-'.join(salary_net) + '€/mon.Net'
+                    elif salary_numbers and 'Net' in salary_text:
+                        salary = '-'.join(salary_numbers) + '€/mon.Net'
+                    else:
+                        salary = salary_text
+
+                job_details_log_message = f"{i}. Job Title: {title}, Location: {location}, Company: {company}, Views: {views}, Applied: {applied}, Salary: {salary}"
+                job_details_logger.info(job_details_log_message)
+
             else:
-                logging.error(f"Failed to fetch job details from {job_url}")
-
-            time.sleep(config['delay'])  # Delay as per the configuration
+                main_logger.error(f"Failed to fetch job details from {job_url}")
+            time.sleep(config['delay'])
         else:
-            logging.warning(f"No job link found in job element. Element HTML: {job_element}")
-else:
-    logging.error("Failed to fetch the search page.")
+            main_logger.warning("No job link found in job element.")
 
-logging.info("Scraper finished.")
+main_logger.info("Scraper finished.")
